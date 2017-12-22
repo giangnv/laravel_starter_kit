65dfa82fbd78a3caa7f1b2d01264594f7c4ab816
Giang Nguyen <giangnv@vnext.vn>
2017-12-22 09:51:41 +0700

    use Curl

--- app/Listeners/SendDictionaryNotify.php
+++ app/Listeners/SendDictionaryNotify.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Storage;
+use Ixudra\Curl\Facades\Curl;
 
 class SendDictionaryNotify
 {
