f8c435db20a85a99dbef6db91b86b2eb279f13d8
Giang Nguyen <giangnv@vnext.vn>
2018-01-05 10:59:43 +0700

    Remove language nav

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -20,15 +20,6 @@
         <div class="navbar-custom-menu">
             <ul class="nav navbar-nav">
 
-                @if (config('locale.status') && count(config('locale.languages')) > 1)
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            <i class="fa fa-language"></i> {{ trans('menus.language-picker.language') }} <span class="caret"></span>
-                        </a>
-                        @include('includes.partials.lang')
-                    </li>
-                @endif
-
                 <li class="dropdown messages-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-envelope-o"></i>
